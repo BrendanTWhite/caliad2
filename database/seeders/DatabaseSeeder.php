af235b605504eb31a6ea9ef472e2ee01dadbc29c
Brendan T White <brendan@thespia.com>
2023-08-30 20:43:27 +0930

    create Brendan

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Brendan',
+            'email' => 'brendan@thespia.com',
+            'password' => bcrypt('dingodingo'),
+        ]);
     }
 }
